29438b57e1cd5a44fe68d683af01440cbd83a2dc
pengppeng <pengpeng@zz-med.com>
2023-03-13 16:30:08 +0800

    修改配置

---
+++ assets/develop_global.md.75d5c7d0.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,N as l}from"./chunks/framework.f5b2a8ed.js";const d=JSON.parse('{"title":"全局组件引入","description":"","frontmatter":{},"headers":[],"relativePath":"develop/global.md"}'),o={name:"develop/global.md"},p=l("",7),e=[p];function t(c,r,F,D,y,i){return a(),n("div",null,e)}const A=s(o,[["render",t]]);export{d as __pageData,A as default};
---
+++ assets/develop_index.md.bcd87a71.js
@@ -0,0 +1,12 @@
+import{_ as s,c as a,o as n,N as e}from"./chunks/framework.f5b2a8ed.js";const y=JSON.parse('{"title":"目录结构","description":"","frontmatter":{},"headers":[],"relativePath":"develop/index.md"}'),p={name:"develop/index.md"},l=e(`<h1 id="目录结构" tabindex="-1">目录结构 <a class="header-anchor" href="#目录结构" aria-hidden="true">#</a></h1><br><br><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#A6ACCD;">-- packages</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">  -- components 本地开发组件库</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">-- play 测试开发组件库的Vue3项目目录</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">-- site 组件库文档站点目录</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),o=[l];function t(c,r,i,d,C,A){return n(),a("div",null,o)}const h=s(p,[["render",t]]);export{y as __pageData,h as default};
